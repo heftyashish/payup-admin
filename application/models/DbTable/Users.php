6772d6bbf733c17c71a870750c0d4ede862cf2a4
KC <gandhi@linuxmint.linuxmint-domain>
2013-01-16 17:19:57 +0530

    admin panel deploy

--- application/models/DbTable/Users.php
+++ application/models/DbTable/Users.php
@@ -14,4 +14,14 @@ class Application_Model_DbTable_Users extends Zend_Db_Table_Abstract
 		else
 			return 'error';
 		}	
+
+    public function users_between($from,$to) {
+
+			$var = $this->select()->from($this,array('id','firstname','lastname','email','status','created_at','updated_at'))->where('created_at= ?', $from)->where('created_at= ?', $to);
+			$row = $this->fetchAll($var);
+		if(!empty($row))
+			return $row ;
+		else
+			return 'error';
+		}			
 }
