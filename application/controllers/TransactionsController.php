c0fb8e7404d4cb29d14c329002f8098753c42fa9
kunal <joshi.kunal@iplaup.com>
2013-01-22 17:48:06 +0530

    transactions

--- application/controllers/TransactionsController.php
+++ application/controllers/TransactionsController.php
@@ -8,14 +8,24 @@ class TransactionsController extends BaseController{
  }
 
 public function transactiondetailsAction(){
-		 $index = new Application_Model_DbTable_Transactions();
-         $data = $index->transaction_detail();
-         
-         $this->view->assign('data',$data);
+					
+						$index = new Application_Model_DbTable_Transactions();
+				    $data = $index->transaction_detail();
+						$this->view->assign('data',$data);
 
-}
+						if($this->getRequest()->isPost()) {
+								if(isset($_POST['submit'])) {
+											$from = $_POST['date_1'];
+											$fromdate = date("Y-m-d",strtotime($from));
+											$to = $_POST['date_2'];
+											$todate = date("Y-m-d",strtotime($to));
 
-
-}
+									$index = new Application_Model_DbTable_Transactions();
+							    $data = $index->transaction_detail_from_date($fromdate,$todate);
+									$this->view->assign('data',$data);		
+									}	
+								}		
+				}
+	}
 
 ?>
--- application/models/DbTable/Transactions.php
+++ application/models/DbTable/Transactions.php
@@ -5,16 +5,51 @@ class Application_Model_DbTable_Transactions extends Zend_Db_Table_Abstract
 
     protected $_name='transactions';
 
+    
+    /* --------------------Transaction details------------------------------------------------------------- */
+
     public function transaction_detail() {
 
-			$var = $this->select()->from($this,array('id','account_id','reference_id','amount','type','status','created_at','updated_at'));
-			$row = $this->fetchAll($var);
-			
+		$select  =  $this->select()->setIntegrityCheck(false);	
+		$select->from('transactions',array('id','account_id','amount','type','status','created_at','updated_at'))
+					 ->join('accounts','accounts.id = transactions.account_id',array('current_balance'))
+					 ->join('users','users.id =accounts.user_id',array('firstname'));
+
+		$row = $this->fetchAll($select)->toArray();
+
 			if(!empty($row))
 					return $row ;
 			else
 					return 'error';
 		}	
+
+
+		 /* --------------------Transaction details by date------------------------------------------------------------- */
+
+		 public function transaction_detail_from_date($fromdate,$todate) {
+
+		$select  =  $this->select()->setIntegrityCheck(false);	
+		$select->from('transactions',array('id','account_id','amount','type','status','created_at','updated_at'))
+					 ->join('accounts','accounts.id = transactions.account_id',array('current_balance'))
+					 ->join('users','users.id =accounts.user_id',array('firstname'));
+		
+					if($fromdate){
+								$select->where('transactions.created_at>= ?',$fromdate);
+							}
+					if($todate){
+								$select->where('transactions.created_at<= ?',$todate);
+							}
+
+		$row = $this->fetchAll($select)->toArray();
+
+			if(!empty($row))
+					return $row ;
+			else
+					return 'error';
+		}	
+
+
+
 }
 
 ?>
