0985a9e01d93c9a9983385047790631cba6970c2
kunal <joshi.kunal@iplaup.com>
2013-01-24 12:15:27 +0530

    transaction deploy

--- application/controllers/TransactionsController.php
+++ application/controllers/TransactionsController.php
@@ -15,13 +15,16 @@ public function transactiondetailsAction(){
 
 						if($this->getRequest()->isPost()) {
 								if(isset($_POST['submit'])) {
+											$type = $_POST['users'];
+				
 											$from = $_POST['date_1'];
 											$fromdate = date("Y-m-d",strtotime($from));
+										
 											$to = $_POST['date_2'];
 											$todate = date("Y-m-d",strtotime($to));
 
 									$index = new Application_Model_DbTable_Transactions();
-							    $data = $index->transaction_detail_from_date($fromdate,$todate);
+							    $data = $index->transaction_detail_from_date($fromdate,$todate,$type);
 									$this->view->assign('data',$data);		
 									}	
 								}		
--- application/models/DbTable/Transactions.php
+++ application/models/DbTable/Transactions.php
@@ -26,7 +26,7 @@ class Application_Model_DbTable_Transactions extends Zend_Db_Table_Abstract
 
 		 /* --------------------Transaction details by date------------------------------------------------------------- */
 
-		 public function transaction_detail_from_date($fromdate,$todate) {
+		 public function transaction_detail_from_date($fromdate,$todate,$type) {
 
 		$select  =  $this->select()->setIntegrityCheck(false);	
 		$select->from('transactions',array('id','account_id','amount','type','status','created_at','updated_at'))
@@ -39,6 +39,9 @@ class Application_Model_DbTable_Transactions extends Zend_Db_Table_Abstract
 					if($todate){
 								$select->where('transactions.created_at<= ?',$todate);
 							}
+					if($type){
+								$select->where('transactions.type= ?',$type);
+						}		
 
 		$row = $this->fetchAll($select)->toArray();
 
